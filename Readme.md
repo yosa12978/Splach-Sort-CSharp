2031efa67cbed3dc364897a1624322b1ac4377fd
yosa <45029926+yosa12978@users.noreply.github.com>
2020-04-13 20:22:49 +0300

    1.0.0

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Spash Sort Algorithm
+splash sort is an algorithm which i created with myself
+## Realization
+
+C#:
+''' 
+        public static void sort(int[] arr)
+        {
+            for(int i = 0; i <= arr.Length-1; i++)
+                for(int j = 0; j <= arr.Length-1; j++)
+                    if(arr[i] < arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+        }
+'''
---
+++ SplashSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SplashSort
+{
+    class Program
+    {
+        public static void sort(int[] arr)
+        {
+            for(int i = 0; i <= arr.Length-1; i++)
+                for(int j = 0; j <= arr.Length-1; j++)
+                    if(arr[i] < arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+        }
+
+        public static void printArray(int[] arr)
+        {
+            foreach(int i in arr)
+                Console.Write($"{i} ");
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Random random = new Random();
+            int[] unsorted_arr = new int[10];
+            for(int i = 0; i <= 9; i++)
+                unsorted_arr[i] = random.Next(1, 100);
+            printArray(unsorted_arr);
+            Console.WriteLine();
+            sort(unsorted_arr);
+            printArray(unsorted_arr);
+            Console.ReadKey();
+        }
+    }
+}
